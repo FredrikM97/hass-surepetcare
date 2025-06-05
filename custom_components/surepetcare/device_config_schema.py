719324a35a470ea4f389a4911be54cd48733ab86
Fredrik <fredrik.martensson@ziklo.com>
2025-06-05 11:53:40 +0000

    Feeder should not have location

--- custom_components/surepetcare/device_config_schema.py
+++ custom_components/surepetcare/device_config_schema.py
@@ -6,10 +6,7 @@ from surepetcare.enums import ProductId
 
 DEVICE_CONFIG_SCHEMAS = {
     ProductId.FEEDER_CONNECT: {
-        "schema": {
-            Required("location_inside"): str,
-            Required("location_outside"): str,
-        },
+        "schema": None,
     },
     ProductId.DUAL_SCAN_PET_DOOR: {
         "schema": {
