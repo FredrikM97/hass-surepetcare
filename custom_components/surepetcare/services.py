fe5c019ac131f1edcb8b136855e92cd2d9e625c7
Fredrik <fredrik.martensson@ziklo.com>
2025-09-09 15:48:54 +0000

    Update debug service to surepcio

--- custom_components/surepetcare/services.py
+++ custom_components/surepetcare/services.py
@@ -14,10 +14,10 @@ def service(name):
 @service("disable_debug_logging")
 async def async_disable_debug_logging(call):
     logging.getLogger("custom_components.surepetcare").setLevel(logging.INFO)
-    logging.getLogger("py_surepetcare").setLevel(logging.INFO)
+    logging.getLogger("surepcio").setLevel(logging.INFO)
 
 
 @service("enable_debug_logging")
 async def async_enable_debug_logging(call):
     logging.getLogger("custom_components.surepetcare").setLevel(logging.DEBUG)
-    logging.getLogger("py_surepetcare").setLevel(logging.DEBUG)
+    logging.getLogger("surepcio").setLevel(logging.DEBUG)
