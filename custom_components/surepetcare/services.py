8415be0eb4ad9ba15dcce08c07bc881bae176afe
Fredrik <fredrik.martensson@ziklo.com>
2025-09-09 12:41:35 +0000

    fix lint

--- custom_components/surepetcare/services.py
+++ custom_components/surepetcare/services.py
@@ -2,16 +2,20 @@ import logging
 
 _service_registry = []
 
+
 def service(name):
     def decorator(func):
         _service_registry.append((name, func))
         return func
+
     return decorator
+
 
 @service("disable_debug_logging")
 async def async_disable_debug_logging(call):
     logging.getLogger("custom_components.surepetcare").setLevel(logging.INFO)
     logging.getLogger("py_surepetcare").setLevel(logging.INFO)
+
 
 @service("enable_debug_logging")
 async def async_enable_debug_logging(call):
