422a4c78e926a574bb6b66c42e4495edb53dea4a
Fredrik <fredrik.martensson@ziklo.com>
2025-09-18 12:35:15 +0200

    lint fix

--- custom_components/surepetcare/device_config_schema.py
+++ custom_components/surepetcare/device_config_schema.py
@@ -6,13 +6,15 @@ from voluptuous import Optional
 # Add default values in case new ProductIds are added in the library
 DEVICE_CONFIG_SCHEMAS = {pid: None for pid in ProductId}
 
-DEVICE_CONFIG_SCHEMAS.update({
-    ProductId.DUAL_SCAN_CONNECT: {
-        Optional("location_inside"): str,
-        Optional("location_outside"): str,
-    },
-    ProductId.DUAL_SCAN_PET_DOOR: {
-        Optional("location_inside"): str,
-        Optional("location_outside"): str,
-    },
-})
+DEVICE_CONFIG_SCHEMAS.update(
+    {
+        ProductId.DUAL_SCAN_CONNECT: {
+            Optional("location_inside"): str,
+            Optional("location_outside"): str,
+        },
+        ProductId.DUAL_SCAN_PET_DOOR: {
+            Optional("location_inside"): str,
+            Optional("location_outside"): str,
+        },
+    }
+)
