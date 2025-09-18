da14dbad0206c658b54d0578716f7e1a5f6a036a
Fredrik <fredrik.martensson@ziklo.com>
2025-09-18 12:33:22 +0200

    Add all ProductId with default value and allow override

--- custom_components/surepetcare/device_config_schema.py
+++ custom_components/surepetcare/device_config_schema.py
@@ -3,8 +3,10 @@
 from surepcio.enums import ProductId
 from voluptuous import Optional
 
-DEVICE_CONFIG_SCHEMAS = {
-    ProductId.FEEDER_CONNECT: None,
+# Add default values in case new ProductIds are added in the library
+DEVICE_CONFIG_SCHEMAS = {pid: None for pid in ProductId}
+
+DEVICE_CONFIG_SCHEMAS.update({
     ProductId.DUAL_SCAN_CONNECT: {
         Optional("location_inside"): str,
         Optional("location_outside"): str,
@@ -13,6 +15,4 @@ DEVICE_CONFIG_SCHEMAS = {
         Optional("location_inside"): str,
         Optional("location_outside"): str,
     },
-    ProductId.HUB: None,
-    ProductId.PET: None,
-}
+})
