e530185d552bd76eee487d6a703e35729e1b5b78
Fredrik Mårtensson <fredrik9779@gmail.com>
2025-09-22 23:17:04 +0200

    Add entities for curfew to pet door

--- custom_components/surepetcare/select.py
+++ custom_components/surepetcare/select.py
@@ -62,7 +62,14 @@ SELECTS: dict[str, tuple[SurePetCareSelectEntityDescription, ...]] = {
             options=[e.name for e in FlapLocking],
             enum_class=FlapLocking,
         ),
-    ),
+    ), 
+    ProductId.PET_DOOR: (
+        SurePetCareSelectEntityDescription(
+            key="curfew_enabled",
+            field="control.curfew.enabled",
+            options=[True, False]
+        ),
+    )  
 }
 
 
