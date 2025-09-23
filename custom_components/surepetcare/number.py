fed9319143c5006ec308675a7422ea14ed6b2d29
Fredrik <fredrik.martensson@ziklo.com>
2025-09-23 09:13:54 +0000

    Add number entitiy

---
+++ custom_components/surepetcare/number.py
@@ -0,0 +1,89 @@
+"""Support for Sure Petcare number entity."""
+
+from dataclasses import dataclass
+import logging
+
+from surepcio import SurePetcareClient
+from homeassistant.components.number import (
+    NumberEntity,
+    NumberEntityDescription,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+
+from .const import (
+    COORDINATOR,
+    COORDINATOR_DICT,
+    DOMAIN,
+    KEY_API,
+)
+from .coordinator import SurePetCareDeviceDataUpdateCoordinator
+from .entity import (
+    SurePetCareBaseEntity,
+    SurePetCareBaseEntityDescription,
+)
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True, kw_only=True)
+class SurePetCareNumberEntityDescription(
+    SurePetCareBaseEntityDescription, NumberEntityDescription
+):
+    """Describes SurePetCare number entity."""
+
+
+SENSORS: dict[str, tuple[SurePetCareNumberEntityDescription, ...]] = {}
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddConfigEntryEntitiesCallback,
+) -> None:
+    """Set up SurePetCare sensors for each matching device."""
+    coordinator_data = hass.data[DOMAIN][config_entry.entry_id][COORDINATOR]
+    client = coordinator_data[KEY_API]
+
+    entities = []
+    for device_id, device_coordinator in coordinator_data[COORDINATOR_DICT].items():
+        descriptions = SENSORS.get(device_coordinator.product_id, ())
+        for description in descriptions:
+            entities.append(
+                SurePetCareNumber(
+                    device_coordinator,
+                    client,
+                    description=description,
+                )
+            )
+    async_add_entities(entities, update_before_add=True)
+
+
+class SurePetCareNumber(SurePetCareBaseEntity, NumberEntity):
+    """The platform class required by Home Assistant."""
+
+    entity_description: SurePetCareNumberEntityDescription
+
+    def __init__(
+        self,
+        device_coordinator: SurePetCareDeviceDataUpdateCoordinator,
+        client: SurePetcareClient,
+        description: SurePetCareNumberEntityDescription,
+    ) -> None:
+        """Initialize a Surepetcare Number Entity."""
+        super().__init__(
+            device_coordinator=device_coordinator,
+            client=client,
+        )
+        self.entity_description = description
+        self._attr_name = description.name
+        self._attr_unique_id = f"{device_coordinator.device_id}-{description.key}"
+        self._attr_native_unit_of_measurement = description.native_unit_of_measurement
+        self._attr_min_value = (
+            description.min_value if hasattr(description, "min_value") else None
+        )
+        self._attr_max_value = (
+            description.max_value if hasattr(description, "max_value") else None
+        )
+        self._attr_step = description.step if hasattr(description, "step") else None
