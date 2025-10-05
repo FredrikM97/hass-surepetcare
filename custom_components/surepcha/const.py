fb987e182d3215f9ada4371b76572b38fbe1815f
Fredrik Mårtensson <fredrik9779@gmail.com>
2025-10-05 15:23:54 +0000

    Updated factory and key_api const

--- custom_components/surepcha/const.py
+++ custom_components/surepcha/const.py
@@ -7,9 +7,9 @@ CONF_PASSWORD = "password"
 
 TOKEN = "token"
 CLIENT_DEVICE_ID = "client_device_id"
-FACTORY = "surepetcare_factory"
+FACTORY = f"{DOMAIN}_factory"
 TIMEOUT_API = 10
-KEY_API = "surepetcare_api"
+KEY_API = f"{DOMAIN}_api"
 COORDINATOR_LIST = "coordinator_list"
 COORDINATOR_DICT = "coordinator_dict"
 COORDINATOR = "coordinator"
